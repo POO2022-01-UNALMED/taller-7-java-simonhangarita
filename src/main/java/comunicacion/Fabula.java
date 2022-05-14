7df88efa9c41bc597f4029466585274cfe3159f1
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-05-14 14:19:52 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -2,9 +2,9 @@ package comunicacion;
 public class Fabula extends Escrito{
   String ensenanza;
   String interpretacion;
-  public Fabula(String origen, String titulo, String autor, int paginas,String enseñanza,String interpretacion){
+  public Fabula(String origen, String titulo, String autor, int paginas,String ensenanza,String interpretacion){
     super(origen,titulo,autor,paginas);
-    this.ensenanza= enseñanza;
+    this.ensenanza= ensenanza;
     this.interpretacion=interpretacion;
   }
   public void setInterpretacion(String idea){
