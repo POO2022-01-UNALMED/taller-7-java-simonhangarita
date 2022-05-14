0df4c2125694c694037b92d22984d7155367255b
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-05-14 14:17:56 -0500

    Create Fabula.java

---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,37 @@
+package comunicacion;
+public class Fabula extends Escrito{
+  String ensenanza;
+  String interpretacion;
+  public Fabula(String origen, String titulo, String autor, int paginas,String enseñanza,String interpretacion){
+    super(origen,titulo,autor,paginas);
+    this.ensenanza= enseñanza;
+    this.interpretacion=interpretacion;
+  }
+  public void setInterpretacion(String idea){
+    this.interpretacion=idea;
+  }
+  public void setEnsenanza(String idea){
+    this.ensenanza=idea;
+  }
+  public String getInterpretacion(){
+    return interpretacion;
+  }
+  public String getEnsenanza(){
+    return ensenanza;
+  }
+  public String interpretacion(){
+    return interpretacion;
+  }
+  public String toString(){
+    String r = this.getOrigen() + "\n";
+		r += this.getTitulo() + "\n";
+		r += this.getAutor() + "\n";
+		r += this.getPaginas()+ "\n";
+    r +=this.ensenanza ;
+    return r;
+  }
+  public int palabrasTotales(int palabrasPagina){
+    return (palabrasPagina);
+  }
+  
+}
